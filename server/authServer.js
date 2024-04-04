4eb992fbcdd63a175613b9024d0032b481142d22
michaelperryjnr <michaelperryt97@gmail.com>
2024-04-04 11:51:18 +0000

    Authentication server

---
+++ server/authServer.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  
+})
+
+app.post("/login", (req, res) => {
+  //Authenticate User
+
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = generateAccessToken(user)
+
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  res.json({ accessToken: accessToken, refreshToken : refreshToken });
+});
+
+function generateAccessToken(user){
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn : '15s' })
+}
+
+app.listen(4000);
