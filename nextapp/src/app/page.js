6826a05ab5bda2d07b38b3e4dc630072a856873b
michaelperryjnr <michaelperryt97@gmail.com>
2024-04-10 02:20:21 +0000

    Page router

---
+++ nextapp/src/app/page.js
@@ -0,0 +1,12 @@
+import Format from "@/layout/format";
+
+/**Components */
+import Section1 from "@/components/section1"
+
+export default function Home() {
+  return (
+    <Format>
+      <Section1></Section1>
+    </Format>
+  );
+}
